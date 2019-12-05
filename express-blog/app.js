7126217dcf3d2bb8abf8e3dedb83285dde4bcdc6
daochun <>
2019-12-05 20:53:43 +0800

    express博客

---
+++ express-blog/app.js
@@ -0,0 +1,53 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+
+
+const blogRouter = require('./routes/blog');
+const userRouter = require('./routes/user');
+
+var app = express();
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+const redisClient = require('./db/redis')
+const sessionStore = new RedisStore({
+  client: redisClient
+})
+
+app.use(session({
+  secret: "akjdf*234#",
+  cookie: {
+    path: "/", // 默认配置
+    httpOnly: true,// 默认配置
+    maxAge: 24 * 60 * 60 * 1000
+  },
+  store: sessionStore
+}));
+
+app.use('/api/blog', blogRouter);
+app.use('/api/user', userRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'dev' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+});
+
+module.exports = app;
---
+++ express-blog/controller/user.js
@@ -0,0 +1,25 @@
+const { exec, escape } = require('../db/mysql')
+const { genPassword } = require('../utils/cryp')
+
+const login = (username, password) => {
+    username = escape(username)
+
+    // 生成加密密码
+    password = genPassword(password)
+    password = escape(password)
+
+    let sql = `select username, realname from users where username=${username} and password=${password}`
+
+    return exec(sql).then(rows => {
+        return rows[0] || {}
+    })
+
+    // if (username === "zhangsan"&& password === "123") {
+    //     return true
+    // }
+    // return false
+}
+
+module.exports = {
+    login
+}
